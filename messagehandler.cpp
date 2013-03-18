e1dde057c7e23589b281681582ec93933cbf42b3
Adam Baker <adamb924@gmail.com>
2013-03-18 09:28:11 +0430

    Error reporting without messing up the HTML formatting.

--- messagehandler.cpp
+++ messagehandler.cpp
@@ -11,5 +11,5 @@ MessageHandler::MessageHandler(const QString & source, QObject *parent) :
 
 void MessageHandler::handleMessage( QtMsgType type, const QString & description, const QUrl & identifier, const QSourceLocation & sourceLocation )
 {
-    QMessageBox::warning(0, tr("XQuery Error"), QString("%1:\n%2 (Line %3, Column %4)").arg(mSource).arg(description).arg(sourceLocation.line()).arg(sourceLocation.column()));
+    QMessageBox::warning(0, mSource, QString("%1 (Line %2, Column %3)").arg(description).arg(sourceLocation.line()).arg(sourceLocation.column()));
 }
