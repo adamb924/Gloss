4bc57ed8d8c0190fd114b0cd6654ffa741f565f1
adamb924 <adamb924@gmail.com>
2014-07-05 10:28:14 -0500

    GUIDs for Allomorphs
    Also, moving definitions of static functions to the implementation where
    they belong.

--- allomorph.cpp
+++ allomorph.cpp
@@ -4,24 +4,36 @@
 #include <QtDebug>
 
 
-Allomorph::Allomorph() :
-    mType(Allomorph::Null), mTextBit( TextBit() ), mId(-1)
+Allomorph::Allomorph(QUuid guid) :
+    mType(Allomorph::Null), mTextBit( TextBit() ), mId(-1), mGuid(guid)
 {
+    if( mGuid.isNull() )
+    {
+        QUuid::createUuid();
+    }
 }
 
-Allomorph::Allomorph(qlonglong id, const TextBit & bit, Type type ) :
-    mType(type), mTextBit(bit), mId(id)
+Allomorph::Allomorph(qlonglong id, const TextBit & bit, Type type , QUuid guid) :
+    mType(type), mTextBit(bit), mId(id), mGuid(guid)
 {
+    if( mGuid.isNull() )
+    {
+        QUuid::createUuid();
+    }
 }
 
-Allomorph::Allomorph(qlonglong id, const TextBit & bit, const TextBitHash & glosses , Type type ) :
-    mType(type), mTextBit(bit), mId(id)
+Allomorph::Allomorph(qlonglong id, const TextBit & bit, const TextBitHash & glosses , Type type, QUuid guid  ) :
+    mType(type), mTextBit(bit), mId(id), mGuid(guid)
 {
     mGlosses.unite(glosses);
+    if( mGuid.isNull() )
+    {
+        QUuid::createUuid();
+    }
 }
 
 Allomorph::Allomorph(const Allomorph & other) :
-    mType(other.mType), mTextBit(other.mTextBit), mId(other.mId), mGlosses(other.mGlosses)
+    mType(other.mType), mTextBit(other.mTextBit), mId(other.mId), mGlosses(other.mGlosses), mGuid(other.mGuid)
 {
 }
 
@@ -31,12 +43,13 @@ Allomorph& Allomorph::operator=(const Allomorph & other)
     mId = other.mId;
     mTextBit = other.mTextBit;
     mGlosses = other.mGlosses;
+    mGuid = other.mGuid;
     return *this;
 }
 
 bool Allomorph::operator==(const Allomorph & other) const
 {
-    return mType == other.mType && mTextBit == other.mTextBit && mId == other.mId && mGlosses == other.mGlosses;
+    return mType == other.mType && mTextBit == other.mTextBit && mId == other.mId && mGlosses == other.mGlosses && mGuid == other.mGuid;
 }
 
 QString Allomorph::typeFormattedString() const
@@ -129,4 +142,141 @@ bool Allomorph::isClitic() const
 void Allomorph::setType( Type t )
 {
     mType = t;
+}
+
+QString Allomorph::guid() const
+{
+    return mGuid.toString();
+}
+
+QString Allomorph::getTypeString(Allomorph::Type t)
+{
+    switch(t)
+    {
+    case Stem:
+        return "Stem";
+    case Prefix:
+        return "Prefix";
+    case Suffix:
+        return "Suffix";
+    case Infix:
+        return "Infix";
+    case BoundStem:
+        return "Bound Stem";
+    case Proclitic:
+        return "Proclitic";
+    case Enclitic:
+        return "Enclitic";
+    case Simulfix:
+        return "Simulfix";
+    case Suprafix:
+        return "Suprafix";
+    case Null:
+    default:
+        return "Null";
+    }
+}
+
+Allomorph::Type Allomorph::getType(QString t)
+{
+    if( t == "Stem" )
+        return Stem;
+    else if( t == "Prefix" )
+        return Prefix;
+    else if( t == "Suffix" )
+        return Suffix;
+    else if( t == "Infix" )
+        return Infix;
+    else if( t == "Bound Stem" )
+        return BoundStem;
+    else if( t == "Proclitic" )
+        return Proclitic;
+    else if( t == "Enclitic" )
+        return Enclitic;
+    else if( t == "Simulfix" )
+        return Simulfix;
+    else if( t == "Suprafix" )
+        return Suprafix;
+    else
+        return Null;
+}
+
+QString Allomorph::getTypeFormatTextString(const QString &text, Allomorph::Type t)
+{
+    switch(t)
+    {
+    case Stem:
+        return text;
+        break;
+    case Prefix:
+        return text + "-";
+        break;
+    case Suffix:
+        return "-" + text;
+        break;
+    case Infix:
+        return "-" + text + "-";
+        break;
+    case BoundStem:
+        return "*" + text;
+        break;
+    case Proclitic:
+        return text + "=";
+        break;
+    case Enclitic:
+        return "=" + text;
+        break;
+    case Simulfix:
+        return "=" + text + "=";
+        break;
+    case Suprafix:
+        return "~" + text + "~";
+        break;
+    case Null:
+        return text;
+        break;
+    }
+    return text;
+}
+
+Allomorph::Type Allomorph::typeFromFormattedString(const QString &string)
+{
+    QRegExp rePrefix("^[^-].*-$");
+    QRegExp reSuffix("^-.*[^-]$");
+    QRegExp reInfix("^-.*-$");
+    QRegExp reBoundStem("^\\*.*$");
+    QRegExp reProclitic("^[^=].*={1,2}$");
+    QRegExp reEnclitic("^={1,2}.*[^=]$");
+    QRegExp reSimulfix("^={1,2}.*={1,2}$");
+    QRegExp reSuprafix("^~.*~$");
+
+    if( rePrefix.exactMatch( string ) )
+        return Allomorph::Prefix;
+    else if( reSuffix.exactMatch( string ) )
+        return Allomorph::Suffix;
+    else if( reInfix.exactMatch( string ) )
+        return Allomorph::Infix;
+    else if( reBoundStem.exactMatch( string ) )
+        return Allomorph::BoundStem;
+    else if( reProclitic.exactMatch( string ) )
+        return Allomorph::Proclitic;
+    else if( reEnclitic.exactMatch( string ) )
+        return Allomorph::Enclitic;
+    else if( reSimulfix.exactMatch( string ) )
+        return Allomorph::Simulfix;
+    else if( reSuprafix.exactMatch( string ) )
+        return Allomorph::Suprafix;
+    else
+        return Allomorph::Stem;
+}
+
+QString Allomorph::stripPunctuation(const QString &string)
+{
+    QString s = string;
+    s.replace("=","");
+    s.replace("~","");
+    s.replace("=","");
+    s.replace("*","");
+    s.replace("-","");
+    return s;
 }
--- allomorph.h
+++ allomorph.h
@@ -12,15 +12,16 @@
 #include "textbit.h"
 
 #include <QRegExp>
+#include <QUuid>
 
 class Allomorph
 {
 public:
     enum Type { Stem,Prefix,Suffix,Infix,BoundStem,Proclitic,Enclitic,Simulfix,Suprafix,Null };
 
-    Allomorph();
-    Allomorph(qlonglong id, const TextBit & bit, Type type );
-    Allomorph(qlonglong id, const TextBit & bit, const TextBitHash & glosses , Type type );
+    Allomorph(QUuid guid = QUuid());
+    Allomorph(qlonglong id, const TextBit & bit, Type type, QUuid guid = QUuid() );
+    Allomorph(qlonglong id, const TextBit & bit, const TextBitHash & glosses , Type type , QUuid guid = QUuid());
     Allomorph(const Allomorph & other);
     Allomorph& operator=(const Allomorph & other);
 
@@ -71,148 +72,29 @@ public:
     //! \brief Set the type of the allomorph to \a t
     void setType( Type t );
 
+    QString guid() const;
+
     //! \brief Returns the type string for \a t
-    static QString getTypeString(Type t)
-    {
-        switch(t)
-        {
-        case Stem:
-            return "Stem";
-        case Prefix:
-            return "Prefix";
-        case Suffix:
-            return "Suffix";
-        case Infix:
-            return "Infix";
-        case BoundStem:
-            return "Bound Stem";
-        case Proclitic:
-            return "Proclitic";
-        case Enclitic:
-            return "Enclitic";
-        case Simulfix:
-            return "Simulfix";
-        case Suprafix:
-            return "Suprafix";
-        case Null:
-        default:
-            return "Null";
-        }
-    }
+    static QString getTypeString(Type t);
 
     //! \brief Returns the Type, given its string represenation \a t
-    static Type getType(QString t)
-    {
-        if( t == "Stem" )
-            return Stem;
-        else if( t == "Prefix" )
-            return Prefix;
-        else if( t == "Suffix" )
-            return Suffix;
-        else if( t == "Infix" )
-            return Infix;
-        else if( t == "Bound Stem" )
-            return BoundStem;
-        else if( t == "Proclitic" )
-            return Proclitic;
-        else if( t == "Enclitic" )
-            return Enclitic;
-        else if( t == "Simulfix" )
-            return Simulfix;
-        else if( t == "Suprafix" )
-            return Suprafix;
-        else
-            return Null;
-    }
+    static Type getType(QString t);
 
     //! \brief Returns a string representation of \a text, as if it were an allomorph of type \a t
-    static QString getTypeFormatTextString( const QString & text, Type t )
-    {
-        switch(t)
-        {
-        case Stem:
-            return text;
-            break;
-        case Prefix:
-            return text + "-";
-            break;
-        case Suffix:
-            return "-" + text;
-            break;
-        case Infix:
-            return "-" + text + "-";
-            break;
-        case BoundStem:
-            return "*" + text;
-            break;
-        case Proclitic:
-            return text + "=";
-            break;
-        case Enclitic:
-            return "=" + text;
-            break;
-        case Simulfix:
-            return "=" + text + "=";
-            break;
-        case Suprafix:
-            return "~" + text + "~";
-            break;
-        case Null:
-            return text;
-            break;
-        }
-        return text;
-    }
+    static QString getTypeFormatTextString( const QString & text, Type t );
 
     //! \brief Returns the Type of an allomorph, given the string representation \a string
-    static Type typeFromFormattedString(const QString & string)
-    {
-        QRegExp rePrefix("^[^-].*-$");
-        QRegExp reSuffix("^-.*[^-]$");
-        QRegExp reInfix("^-.*-$");
-        QRegExp reBoundStem("^\\*.*$");
-        QRegExp reProclitic("^[^=].*={1,2}$");
-        QRegExp reEnclitic("^={1,2}.*[^=]$");
-        QRegExp reSimulfix("^={1,2}.*={1,2}$");
-        QRegExp reSuprafix("^~.*~$");
-
-        if( rePrefix.exactMatch( string ) )
-            return Allomorph::Prefix;
-        else if( reSuffix.exactMatch( string ) )
-            return Allomorph::Suffix;
-        else if( reInfix.exactMatch( string ) )
-            return Allomorph::Infix;
-        else if( reBoundStem.exactMatch( string ) )
-            return Allomorph::BoundStem;
-        else if( reProclitic.exactMatch( string ) )
-            return Allomorph::Proclitic;
-        else if( reEnclitic.exactMatch( string ) )
-            return Allomorph::Enclitic;
-        else if( reSimulfix.exactMatch( string ) )
-            return Allomorph::Simulfix;
-        else if( reSuprafix.exactMatch( string ) )
-            return Allomorph::Suprafix;
-        else
-            return Allomorph::Stem;
-    }
+    static Type typeFromFormattedString(const QString & string);
 
     //! \brief Returns \a string with punctuation stripped out
-    static QString stripPunctuation( const QString & string )
-    {
-        QString s = string;
-        s.replace("=","");
-        s.replace("~","");
-        s.replace("=","");
-        s.replace("*","");
-        s.replace("-","");
-        return s;
-    }
+    static QString stripPunctuation( const QString & string );
 
 private:
     Type mType;
     TextBit mTextBit;
     qlonglong mId;
     TextBitHash mGlosses;
+    QUuid mGuid;
 };
 
 
